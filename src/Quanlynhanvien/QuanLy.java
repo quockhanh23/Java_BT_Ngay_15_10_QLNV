0ce3cbd3e818e05f25b44fc27693a227d8988f76
unknown <yeuthuongvoivangdiqua@gmail.com>
2021-10-16 20:48:55 +0700

    done

--- src/Quanlynhanvien/QuanLy.java
+++ src/Quanlynhanvien/QuanLy.java
@@ -2,9 +2,9 @@ package Quanlynhanvien;
 
 public interface QuanLy<T> {
     void them(T t);
-    void sua(String name, T t);
+    void sua(int id, T t);
     void xoa(int id);
-    int timKiem(String name);
+    int timKiem(int id);
     void sapXep();
     void print();
 
--- src/Quanlynhanvien/QuanLyNhanVien.java
+++ src/Quanlynhanvien/QuanLyNhanVien.java
@@ -1,7 +1,7 @@
 package Quanlynhanvien;
 
 public class QuanLyNhanVien implements QuanLy<NhanVien> {
-    private NhanVien[] danhSachNhanVien = new NhanVien[10];
+    private NhanVien[] danhSachNhanVien = new NhanVien[5];
     private int size = 0;
 
     @Override
@@ -11,20 +11,33 @@ public class QuanLyNhanVien implements QuanLy<NhanVien> {
     }
 
     @Override
-    public void sua(String name, NhanVien nhanVien) {
-        danhSachNhanVien[timKiem(name)] = nhanVien;
+    public void sua(int id, NhanVien nhanVien) {
+        danhSachNhanVien[timKiem(id)] = nhanVien;
     }
 
     @Override
     public void xoa(int id) {
 
+        NhanVien[] xoaDanhSach1 = new NhanVien[danhSachNhanVien.length - 1];
+        for (int i = 0; i < timKiem(id); i++) {
+            xoaDanhSach1[i] = danhSachNhanVien[i];
+        }
+        for (int i = timKiem(id) + 1; i < danhSachNhanVien.length; i++) {
+            xoaDanhSach1[i - 1] = danhSachNhanVien[i];
+        }
+        for (int i = 0; i < xoaDanhSach1.length; i++) {
+            System.out.println(xoaDanhSach1[i]);
+        }
+
     }
 
     @Override
-    public int timKiem(String name) {
+    public int timKiem(int id) {
+
         for (int i = 0; i < size; i++) {
-            if (name == danhSachNhanVien[i].getName()) {
+            if (id == danhSachNhanVien[i].getId()) {
                 return i;
+
             }
         }
         return -1;
@@ -47,18 +60,22 @@ public class QuanLyNhanVien implements QuanLy<NhanVien> {
         NhanVien nv1 = new NhanVien(1, "khánh1", 11, 1);
         NhanVien nv2 = new NhanVien(2, "khánh2", 12, 1);
         NhanVien nv3 = new NhanVien(3, "khánh3", 13, 1);
-        NhanVien nv4 = new NhanVien(4, "khánh3", 15, 1);
+        NhanVien nv4 = new NhanVien(4, "khánh4", 15, 1);
         NhanVien nv5 = new NhanVien(5, "khánh5", 23, 1000);
         ql.them(nv1);
         ql.them(nv2);
         ql.them(nv3);
         ql.them(nv4);
+        ql.them(nv5);
 
         ql.print();
-        NhanVien nv6 = new NhanVien(5, "khánh6", 53, 5000);
-        System.out.println(ql.timKiem("khánh2"));
-        ql.sua("khánh", nv6);
-        ql.print();
+        System.out.println(ql.timKiem(3));
+
+        ql.xoa(5);
+
+
+
+
 
 
     }
